c174df814bbcf96c66c63c7d68e864d7b1e0f30b
Vladislav Antoshkin <vladantochkin@gmail.com>
2021-12-01 17:22:40 +0100

    Base pierre

--- Source/js/ElementScene.js
+++ Source/js/ElementScene.js
@@ -4,7 +4,13 @@
  //
  //********************************************************
 
- function PierreF(r,coulPierre,haut,scene){
+ function PierreF(r,coulPierre,haut,scene,equipe){
+  
+  let coulEquipe;
+  if(equipe)
+    coulEquipe=0xFF0000;
+  else
+    coulEquipe=0x0000FF;
 
   let P0=new THREE.Vector3( 0, 3*haut/2, 0 );
   let P1=new THREE.Vector3( r/2, 3*haut/2, 0 );
@@ -22,58 +28,17 @@
   let ptTab2=[R1,Q1,Q2,Q3];
   let ptTab3=[P3,R0,R1];
 
-  let Lathe=latheBezCub(50, 100, ptTab, coulPierre, 1/2, false);
-  let latheGeometry=Lathe.geometry;
-  let Lathe2=latheBezCub(50, 100, ptTab2, coulPierre, 1/2, false);
-  let latheGeometry2=Lathe2.geometry;	
-  //scene.add(Lathe2);
-  let Lathe3=latheBezQuad(50, 100, ptTab3, coulPierre, 1/2, false);
-  let latheGeometry3=Lathe3.geometry;
-  THREE.GeometryUtils.merge(latheGeometry2,latheGeometry3);
-  THREE.GeometryUtils.merge(latheGeometry,latheGeometry);
-
-  var totalGeom = new THREE.Geometry();
-
-  let BasePierre=surfPhong(latheGeometry2,coulPierre,true,0.5,coulPierre);
-  BasePierre.rotateX(1/2*Math.PI);
-  BasePierre.position.set(BasePierre.position.x,BasePierre.position.y+r,0+3*haut/2);
-  BasePierre.updateMatrix(); 
-  totalGeom.merge(BasePierre.geometry, BasePierre.matrix);
-  let LatheInter=surfPhong(latheGeometry,0xFF0000,true,0.5,0xFF0000);
-  LatheInter.rotateX(1/2*Math.PI);
-  LatheInter.position.set(LatheInter.position.x,LatheInter.position.y+r,0+3*haut/2);
-  LatheInter.updateMatrix(); 
-  totalGeom.merge(LatheInter.geometry, LatheInter.matrix);
-  var materiaux=[];
-  materiaux.push(BasePierre.material);
-  materiaux.push(LatheInter.material);
-  var total = new THREE.Mesh(totalGeom, new THREE.MeshFaceMaterial(materiaux));
-  var total2=_mergeMeshes([BasePierre,LatheInter], true);
-  //total.rotateX(1/2*Math.PI);
-  total2.position.set(0,1,0);
-  //total2.position.set(total2.position.x,total2.position.y,0);
-  //scene.add(BasePierre);
-  //scene.add(LatheInter);
-  scene.add(total2);
-  //BasePierre.rotateX(1/2*Math.PI);
-  //BasePierre.position.set(BasePierre.position.x,BasePierre.position.y+r,0);
-  //scene.add(BasePierre);
-  //scene.add(LatheInter);
-  return total;
-  //scene.add(Lathe3);
+  let Lathe1=latheBezCub(50, 100, ptTab, coulEquipe, 1/2, false);
+  let Lathe2=latheBezQuad(50, 100, ptTab3, coulPierre, 1/2, false);
+  let Lathe3=latheBezCub(50, 100, ptTab2, coulPierre, 1/2, false);
   
-
-  //let LatheCSG=new ThreeBSP(BasePierre);
-  //let LatheCSG2=new ThreeBSP(LatheInter);
-  //let Pierre=LatheCSG.union(LatheCSG2);
-  //let Pierre=Pierre.union(LatheCSG3);
-
-  //PierreGeo = Pierre.toMesh();
-  /*PierreGeo.castShadow = true;
-  PierreGeo.receiveShadow = true;
-  PierreGeo.material = MateriauPhong("#009999",1,true,"#FFFF00");*/
-
-  //scene.add(PierreGeo);
-  //return PierreGeo;
-  //return None;
+  BasePierre = new THREE.Group();
+  BasePierre.add(Lathe1);
+  BasePierre.add(Lathe2);
+  BasePierre.add(Lathe3);
+  BasePierre.rotateX(1/2*Math.PI);
+  BasePierre.position.set(BasePierre.position.x+r,BasePierre.position.y+r,BasePierre.position.z+3*haut/2);
+  
+  scene.add(BasePierre);
+  return BasePierre;
 }
--- Source/js/init.js
+++ Source/js/init.js
@@ -23,43 +23,8 @@ function init(){
   let r= 3/4;                //Rayon de la pierre
   let coulPierre=0x777777;
   let haut=0.30;
-  /*let P0=new THREE.Vector3( 0, 3*haut/2, 0 );
-  let P1=new THREE.Vector3( r/2, 3*haut/2, 0 );
-  let P2=new THREE.Vector3( r, 3*haut/2, 0 );
-  let P3=new THREE.Vector3( r, haut/2, 0 );
-
-  let R0=new THREE.Vector3(r,0,0);
-  let R1=new THREE.Vector3(r, -haut/2, 0);
-
-  let Q1=new THREE.Vector3(r,-3*haut/2,0);
-  let Q2=new THREE.Vector3(r/2,-3*haut/2,0);
-  let Q3=new THREE.Vector3(0,-3*haut/2,0);
-
-  let ptTab=[P0,P1,P2,P3];
-  let ptTab2=[R1,Q1,Q2,Q3];
-  let ptTab3=[P3,R0,R1];
-
-  let Lathe=latheBezCub(50, 50, ptTab, coulPierre, 1/2, false);
-  Lathe.position.set(Lathe.position.x,Lathe.position.y+r,0);
-  Lathe.rotateX(1/2*Math.PI);
-  scene.add(Lathe);
-  let Lathe2=latheBezCub(50, 50, ptTab2, coulPierre, 1/2, false);
-  Lathe2.position.set(Lathe2.position.x,Lathe2.position.y+r,0);
-  Lathe2.rotateX(1/2*Math.PI);
-  scene.add(Lathe2);
-  let Lathe3=latheBezQuad(50, 50, ptTab3, coulPierre, 1/2, false);
-  Lathe3.position.set(Lathe3.position.x,Lathe3.position.y+r,0);
-  Lathe3.rotateX(1/2*Math.PI);
-  scene.add(Lathe3);*/
-  PierreF(r,coulPierre,haut,scene);
-
   
-
-  //let LatheCSG=new ThreeBSP(Lathe);
-  //let LatheCSG2=new ThreeBSP(Lathe2);
-  //let LatheCSG3=new ThreeBSP(Lathe3);
-  //let Pierre=LatheCSG.union(LatheCSG2);
-  //let Pierre=Pierre.union(LatheCSG3);
+  PierreF(r,coulPierre,haut,scene,true);
 
 
 
